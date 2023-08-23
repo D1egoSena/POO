eeadfdc3ab7156808b5db219f9c2b85da121339e
Diego Sena <112528354+D1egoSena@users.noreply.github.com>
2023-08-23 15:43:58 -0300

    Teste.java

---
+++ Atividade-Assíncrona3/Teste.java
@@ -0,0 +1,8 @@
+package Java.src.poo.atividade_assincrona3.model;
+
+public class Teste {
+    public static void main(String[] args) {
+        Funcionario funcionario = new Funcionario("Diego", 2000, 40);
+        funcionario.imprimeDados();
+    }
+}
