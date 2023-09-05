b5acb403142f204f892e7e703325ca35b6285943
Diego Sena <112528354+D1egoSena@users.noreply.github.com>
2023-09-05 19:00:44 -0300

    Add files via upload

---
+++ Operacoes-Bancarias/ContaCorrente.java
@@ -0,0 +1,13 @@
+package OperaçõesBancarias.model;
+
+public class ContaCorrente {
+    private double saldo = 0;
+
+    public void executarOperacao(Operacao opr) {
+        saldo += opr.operar();
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+}
---
+++ Operacoes-Bancarias/Correntista.java
@@ -0,0 +1,19 @@
+package OperaçõesBancarias.model;
+
+public class Correntista {
+    private String nome;
+    private ContaCorrente conta;
+
+    public Correntista(String nome, ContaCorrente conta) {
+        this.nome = nome;
+        this.conta = conta;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public ContaCorrente getContaCorrente() {
+        return conta;
+    }
+}
